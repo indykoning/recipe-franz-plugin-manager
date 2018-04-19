364043761c7b0bed9927cd5c6fa7ca820b372ef1
indy koning <indykoning@gmail.com>
2018-04-19 23:25:29 +0200

    get git repos

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ let { join } = require('path');
 const { lstatSync, readdirSync } = require('fs');
 
 const isDirectory = source => lstatSync(source).isDirectory();
-// const dirContains = (source, folderName) => {console.log(source);readdirSync(source).map(name => {console.log(name);return name === folderName})};
+const isGitRepo = source => readdirSync(source).includes('.git');
 const getDirectories = source =>
-    readdirSync(source).map(name => join(source, name)).filter(isDirectory).map(name => {console.log(name, readdirSync(name));return true});
-
+    readdirSync(source).map(name => join(source, name)).filter(isDirectory && isGitRepo);
 console.log('--------');
 console.log(getDirectories(__dirname + '/../'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
