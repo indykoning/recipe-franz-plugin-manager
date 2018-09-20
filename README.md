12b9140df80fe3647c16436f567732e12327ffc0
Indy Koning <indykoning@hotmail.com>
2018-09-20 21:04:56 +0200

    Added file cache

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Bitbucket Franz Plugin
+# Franz Plugin Installer
 
 Follow instructions [here](https://github.com/meetfranz/plugins/blob/master/docs/integration.md) for installation of this plugin.
+
+##Requirements
+
+Your system needs to have:
+ * Git
+ * NPM
+ * NodeJs
+ 
+ To function correctly.
+
+Once you have placed this in it's correct folder you'll need to run `npm install` to download the dependencies
--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 module.exports = Franz => Franz;
 
 //my code
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-let path = require('path');
-let { join } = require('path');
-const { lstatSync, readdirSync } = require('fs');
-const fs = require('fs');
-let gitRecipes = [];
-let hasScannedRecipes = false;
-const { exec, execSync } = require('child_process');
+const express                       = require('express');
+const bodyParser                    = require('body-parser');
+const app                           = express();
+let path                            = require('path');
+let { join }                        = require('path');
+const { lstatSync, readdirSync }    = require('fs');
+const fs                            = require('fs');
+let gitRecipes                      = [];
+let hasScannedRecipes               = false;
+const { exec, execSync }            = require('child_process');
 (function(){
     // Change directory to parent dir (assumed to be the dev directory with all other plugins)
     process.chdir(__dirname + '/../');
@@ -22,24 +22,42 @@ const { exec, execSync } = require('child_process');
     app.get('/', (req, res) =>
         res.sendFile(path.join(__dirname + '/site/index.html'))
     );
+
     app.get('/api/plugin/list', (req, res) => {
         res.json(getRecipes(recipeDir));
     });
+
     app.post('/api/plugin/add', (req, res) => {
+        fs.unlinkSync(__dirname + '/recipeCache.json');
         res.json({type:'clone',result:git_clone(req.body.repository)});
     });
+
     app.delete('/api/plugin/remove', (req, res) => {
         deleteFolderRecursive(recipeDir + '/' + req.body.recipe);
+        getRecipes(recipeDir, true);
         res.json({type:'remove',result:'successfully removed!'})
     });
 
-    app.listen(3000, () => console.log('Example app listening on port 3000!'));
+    app.listen(3000, () => console.log('App listening on port 3000!'));
 
     function getRecipes(directory, force = false) {
-        if (!hasScannedRecipes || force) {
-            gitRecipes = getGitDirectories(directory);
-            hasScannedRecipes = true;
+        if (gitRecipes.length === 0 || !hasScannedRecipes || force) {
+            if (!force) {
+                try {
+                    gitRecipes = require(__dirname + '/recipeCache.json');
+                } catch (e) {
+                    console.info('scanning', e);
+                    gitRecipes = getGitDirectories(directory);
+                    fs.writeFile(__dirname + '/recipeCache.json', JSON.stringify(gitRecipes), 'utf8', () => {});
+
+                }
+            } else {
+                console.info('scanning forced');
+                gitRecipes = getGitDirectories(directory);
+                fs.writeFile(__dirname + '/recipeCache.json', JSON.stringify(gitRecipes), 'utf8', () => {});
+            }
         }
+        hasScannedRecipes = true;
         return gitRecipes;
     }
 
