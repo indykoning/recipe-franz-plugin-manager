c1a9c70d4f8fe8b71790fc439333f8f2fd5fa62a
indy koning <indykoning@gmail.com>
2018-10-08 23:20:22 +0200

    Added search option & Moved to Bootstrap

--- README.md
+++ README.md
@@ -14,5 +14,4 @@ Your system needs to have:
 Once you have placed this in it's correct folder you'll need to run `npm install` to download the dependencies
 
 ## Ideas
- * Build database of recipies using git repository or maybe crawling the Franz plugins page
  * If possible catch exceptions of franz not finding recipies and attempt to resolve the recipies
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 module.exports = Franz => Franz;
 
+// Get all franz issues: https://api.github.com/repos/meetfranz/plugins/issues
+// Better get all & search franz issues: https://api.github.com/search/issues?q=repo:meetfranz/plugins+https://github.com/%20in:body&per_page=50&page=1
 //my code
 const express                       = require('express');
 const bodyParser                    = require('body-parser');
