0e0ae23957f8fe2f3a55388f0f292466076303e5
indy koning <indykoning@gmail.com>
2018-10-09 21:42:15 +0200

    Performed some much needed code cleanup.

--- index.js
+++ index.js
@@ -2,100 +2,11 @@
 
 module.exports = Franz => Franz;
 
-// Get all franz issues: https://api.github.com/repos/meetfranz/plugins/issues
 // Better get all & search franz issues: https://api.github.com/search/issues?q=repo:meetfranz/plugins+https://github.com/%20in:body&per_page=50&page=1
 //my code
-const express                       = require('express');
-const bodyParser                    = require('body-parser');
-const app                           = express();
-let path                            = require('path');
-let { join }                        = require('path');
-const { lstatSync, readdirSync }    = require('fs');
-const fs                            = require('fs');
-let gitRecipes                      = [];
-let hasScannedRecipes               = false;
-const { exec, execSync }            = require('child_process');
+
 (function(){
-    // Change directory to parent dir (assumed to be the dev directory with all other plugins)
-    process.chdir(__dirname + '/../');
-    let recipeDir = process.cwd();
-    app.use(bodyParser.urlencoded({extended: true}));
-
-    app.get('/', (req, res) =>
-        res.sendFile(path.join(__dirname + '/site/index.html'))
-    );
-
-    app.get('/api/plugin/list', (req, res) => {
-        res.json(getRecipes(recipeDir));
-    });
-
-    app.post('/api/plugin/add', (req, res) => {
-        fs.unlinkSync(__dirname + '/recipeCache.json');
-        res.json({type:'clone',result:git_clone(req.body.repository)});
-    });
-
-    app.delete('/api/plugin/remove', (req, res) => {
-        deleteFolderRecursive(recipeDir + '/' + req.body.recipe);
-        getRecipes(recipeDir, true);
-        res.json({type:'remove',result:'successfully removed!'})
-    });
-
-    app.listen(require(__dirname + '/package.json').config.serviceURL.split(":").pop(), () => {});
-
-    function getRecipes(directory, force = false) {
-        if (gitRecipes.length === 0 || !hasScannedRecipes || force) {
-            if (!force) {
-                try {
-                    gitRecipes = require(__dirname + '/recipeCache.json');
-                } catch (e) {
-                    console.info('scanning', e);
-                    gitRecipes = getGitDirectories(directory);
-                    fs.writeFile(__dirname + '/recipeCache.json', JSON.stringify(gitRecipes), 'utf8', () => {});
-
-                }
-            } else {
-                console.info('scanning forced');
-                gitRecipes = getGitDirectories(directory);
-                fs.writeFile(__dirname + '/recipeCache.json', JSON.stringify(gitRecipes), 'utf8', () => {});
-            }
-        }
-        hasScannedRecipes = true;
-        return gitRecipes;
-    }
-
-    const isDirectory = source => lstatSync(source).isDirectory();
-    const isGitRepo = source => readdirSync(source).includes('.git');
-    const getGitDirectories = source => readdirSync(source)
-        .map(name => join(source, name))
-        .filter(isDirectory && isGitRepo)
-        .map(directory =>
-        {
-            return {
-                directory: directory,
-                friendlyName: require(directory+'/package.json').name
-            };
-        });
-    const deleteFolderRecursive = function(path) {
-        if (fs.existsSync(path)) {
-            fs.readdirSync(path).forEach(function(file, index){
-                var curPath = path + "/" + file;
-                if (fs.lstatSync(curPath).isDirectory()) { // recurse
-                    deleteFolderRecursive(curPath);
-                } else { // delete file
-                    fs.unlinkSync(curPath);
-                }
-            });
-            fs.rmdirSync(path);
-        }
-    };
-    const git_clone = url => {
-        try {
-            execSync('git clone ' + url).toString();
-        }catch (e) {
-            return {error: e.message};
-        }
-        return {};
-    }
+    require(__dirname + '/lib/Server').start();
 })();
 
 
---
+++ site/js/script.js
@@ -0,0 +1,82 @@
+let recipeList = [];
+let xhr = new XMLHttpRequest();
+
+// Load installed plugins.
+xhr.open("GET", "http://localhost:" + window.location.port + "/api/plugin/list");
+xhr.onload = (e) => {
+    recipeList = JSON.parse(e.target.responseText);
+
+    recipeList.forEach(recipe => {
+        $('<div class="row"><div title="' + recipe.directory + '" class="col">' + recipe.friendlyName + '</div><div class="col"><form onsubmit="if(confirm(\'Are you sure you want to uninstall this plugin?\')){submitForm(event)}" class="deleteform float-right" method="delete" action="/api/plugin/remove"><input type="hidden" name="recipe" value="' + recipe.directory.substring(recipe.directory.lastIndexOf("/") + 1) + '"><input type="submit" class="btn btn-danger pull-right" value="delete"></form></div></div>').appendTo('#pluginList');
+    });
+};
+xhr.send();
+
+
+// Get anything containing https://github.com to a newline (A standard people are encouraged to folow)
+const gitRegex = /https:\/\/github\.com\/[^\r\n]+/;
+const items_per_page = 50;
+let page = 1;
+let searchTimeout = null;
+
+document.getElementById('searchfield').addEventListener('input', event => search(event.target.value));
+function search (query) {
+    clearTimeout(searchTimeout);
+    // Show loading animation while waiting for github to respond
+    document.getElementsByClassName('search-results')[0].innerHTML = '<div><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;display: block; margin: auto;" xml:space="preserve"> <rect x="0" y="5.0462" width="4" height="19.9076" fill="#333" opacity="0.2"> <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0s" dur="0.6s" repeatCount="indefinite"></animate> <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0s" dur="0.6s" repeatCount="indefinite"></animate> <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0s" dur="0.6s" repeatCount="indefinite"></animate> </rect> <rect x="8" y="7.5462" width="4" height="14.9076" fill="#333" opacity="0.2"> <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.15s" dur="0.6s" repeatCount="indefinite"></animate> <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite"></animate> <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite"></animate> </rect> <rect x="16" y="9.9538" width="4" height="10.0924" fill="#333" opacity="0.2"> <animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.3s" dur="0.6s" repeatCount="indefinite"></animate> <animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite"></animate> <animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite"></animate> </rect> </svg></div>';
+    searchTimeout = setTimeout(() => {
+        // Get all issues in meetfranz/plugins repo containing github.com in the body and containing the search query.
+        xhr.open("GET", "https://api.github.com/search/issues?q=repo:meetfranz/plugins+https://github.com/%20in:body+" + query + "%20in:body,title&per_page=" + items_per_page + "&page=" + page);
+        xhr.onload = (e) => {
+            document.getElementsByClassName('search-results')[0].innerHTML = "";
+            githubList = JSON.parse(e.target.responseText);
+
+            if (githubList.message != undefined && githubList.items == undefined) {
+                document.getElementsByClassName('search-results')[0].innerText = githubList.message;
+            }
+            if (githubList.items.length == 0) {
+                document.getElementsByClassName('search-results')[0].innerText = "Nothing has been found!";
+            }
+            githubList.items.forEach(post => {
+                let repository = gitRegex.exec(post.body)[0] + ".git";
+
+                $('<div class="row my-1"> <a href="' + post.html_url + '" target="_blank" class="col">' + post.title + '</a> <button onclick="document.getElementById(\'repository\').value = `' + repository + '`" class="col btn btn-primary">Move to install</button></div>').appendTo('.search-results');
+            });
+        };
+        xhr.send();
+    },500);
+};
+
+let responseDOM = document.getElementById('response');
+// General REST form submit.
+function submitForm (e) {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    let body = [...formData.entries()] // expand the elements from the .entries() iterator into an actual array
+        .map(e => encodeURIComponent(e[0]) + "=" + encodeURIComponent(e[1]))  // transform the elements into encoded key-value-pair
+
+    xhr.open(e.target.getAttribute('method') ? e.target.getAttribute('method') : e.target.method, e.target.action, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onload = (e) => {
+        let json = JSON.parse(e.target.responseText);
+        switch (json.type){
+            case 'clone':
+                if(json.result.error !== undefined ) {
+                    responseDOM.className = "alert alert-danger";
+                    responseDOM.innerText = json.result.error;
+                } else {
+                    responseDOM.className = "alert alert-success";
+                    responseDOM.innerText = 'Successfully added! To complete installation you\'ll have to restart Franz.'
+                }
+                break;
+            case 'remove':
+                responseDOM.className = "alert alert-success";
+                responseDOM.innerText = json.result;
+                break;
+            default:
+                responseDOM.className = "alert alert-info";
+                responseDOM.innerText = e.target.responseText;
+        }
+    };
+    xhr.send(body);
+}
